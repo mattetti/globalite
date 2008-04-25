ec6a976ce56a74f0eaf5b1aa20c3db25699c4cea
Piotr Nowak <pnowak@pnowak-laptop.(none)>
2008-04-25 22:43:38 +0200

    fixed localization of model field names. fixed ommited parameters: message and header_message for error_messages_for tag.

--- lib/rails/localized_active_record.rb
+++ lib/rails/localized_active_record.rb
@@ -35,6 +35,41 @@ module ActiveRecord
               })
     end
     
+    # Redefine the ActiveRecord::Errors::full_messages method:
+    #  Returns all the full error messages in an array. 'Base' messages are handled as usual.
+    #  Non-base messages are prefixed with the attribute name as usual UNLESS they begin with '^'
+    #  in which case the attribute name is omitted.
+    #  E.g. validates_acceptance_of :accepted_terms, :message => '^Please accept the terms of service'
+    #  
+    #  
+    #  If field name has the same key like in language yaml file, its replaced by its corresponding language file value.
+    #  This fixes the problem of translating validation messages but not field names. Now you can fully localize them.
+    #  E.g. validates_presence_of :name
+    #  produces (in en-UK and pl-PL:
+    #  Name can't be empty
+    #  Nazwa jest wymagana
+    #  By convetion yaml language key falue for field is the same as ActiveRecords model field name
+    #  If plugin can't find such key, it behaves just like without plugin.
+    def full_messages
+      full_messages = []
+
+      @errors.each_key do |attr|
+        @errors[attr].each do |msg|
+          next if msg.nil?
+
+          if attr == "base"
+            full_messages << msg
+          elsif msg =~ /^\^/
+            full_messages << msg[1..-1]
+          else
+            full_messages << attr.intern.l(attr) + " " + msg
+          end
+        end
+      end
+
+      return full_messages
+    end
+    
     # # Handle model error localization
     # def add(attribute, msg = @@default_error_messages[:invalid])
     #        @errors[attribute.l] = [] if @errors[attribute.to_s].nil?
